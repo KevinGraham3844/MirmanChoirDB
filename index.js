f920a6a258fa128182b05effee7a4b049205a6e6
Kevgraham384 <kev.graham384@gmail.com>
2024-07-21 12:27:52 -0700

    testing port configs

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use('/api/login', loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
